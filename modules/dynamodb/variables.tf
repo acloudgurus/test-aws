f9d5493268839bb0ae4032be30f23b0478673a18
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-07-10 15:03:52 +0530

    Add files via upload

---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,14 @@
+variable "table_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+}
+
+variable "hash_key" {
+  description = "Hash key for the DynamoDB table"
+  type        = string
+}
+
+variable "hash_key_type" {
+  description = "Hash key type"
+  type        = string
+}
